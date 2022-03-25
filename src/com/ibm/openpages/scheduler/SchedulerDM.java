9239b782d5c469f9fa9460569550262aee26cc64
Danny <danny.munoz.ramirez@gmail.com>
2022-03-25 09:59:00 -0400

    Workflow process terminated when scheduler job is executed

--- src/com/ibm/openpages/scheduler/SchedulerDM.java
+++ src/com/ibm/openpages/scheduler/SchedulerDM.java
@@ -1,7 +1,5 @@
 package com.ibm.openpages.scheduler;
 
-
-import com.ibm.openpages.api.Context;
 import com.ibm.openpages.api.metadata.Id;
 import com.ibm.openpages.api.query.IQuery;
 import com.ibm.openpages.api.query.IResultSetRow;
@@ -16,13 +14,22 @@ import com.ibm.openpages.api.service.IResourceService;
 import com.ibm.openpages.api.service.IServiceFactory;
 import com.ibm.openpages.api.service.IWorkflowService;
 import com.ibm.openpages.api.service.local.configuration.CurrentReportingPeriodImpl;
-import com.ibm.openpages.api.workflow.*;
+import com.ibm.openpages.api.workflow.IWFActivity;
+import com.ibm.openpages.api.workflow.IWFActivityInstance;
+import com.ibm.openpages.api.workflow.IWFProcess;
+import com.ibm.openpages.api.workflow.IWFProcessDefinition;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static com.ibm.openpages.api.service.ServiceFactory.*;
-import static org.bouncycastle145.asn1.x509.X509ObjectIdentifiers.id;
+import static com.ibm.openpages.api.service.ServiceFactory.getServiceFactory;
 
+/**
+ * This Class will run when the Scheduler job is executed from UI. It will get all KRI Values with active WorkFlow
+ * instances and fields Collection Status = "Collected" AND FastMapUploaded = "Yes", in order to finalize this
+ * Workflow instance automatically
+ */
 public class SchedulerDM extends AbstractScheduledProcess {
 
     public SchedulerDM() {
@@ -37,74 +44,82 @@ public class SchedulerDM extends AbstractScheduledProcess {
     public void execute() throws Exception {
 
         try {
+            IGRCObject object;
 
-            // Get all KRI Values ID with fastMapUpload = yes and Status = "Collected"
-            String query = "SELECT [KeyRiskIndicatorValue].[Resource ID] FROM [KeyRiskIndicatorValue] " + "WHERE [KeyRiskIndicatorValue].[IKI-KRIValues:FastMapUploaded] = 'Yes' " + "AND [KeyRiskIndicatorValue].[OPSS-KRI-Shared:Collection Status] = 'Collected'";
-
+            // Initializing Query Services
             IServiceFactory iServiceFactory = getServiceFactory(getApplicationContext());
             IResourceService resourceService = iServiceFactory.createResourceService();
 
+            // Initializing Query Services
             IQueryService queryService = iServiceFactory.createQueryService();
-            IQuery iQuery = queryService.buildQuery(query);
+
+            /*
+                Query to get all KRI Values ID with fields
+                fastMapUpload = 'yes' and Collection Status = 'Collected'
+                present in the system
+             */
+            IQuery iQuery = queryService.buildQuery(queryBuilder());
             ITabularResultSet iResultSetRows = iQuery.fetchRows(0);
 
+            // Iterating rows
             for (IResultSetRow row : iResultSetRows) {
 
+                // Getting first column element Object Id
                 IField field = row.getField(0);
                 IIdField id = (IIdField) field;
                 Id value = id.getValue();
 
+                // Creating WF Services
                 IWorkflowService wf = iServiceFactory.createWorkflowService();
 
-                IGRCObject object = resourceService.getGRCObject(value);
-
+                // Setting as 'true' the active workflow visibility
                 GRCObjectFilter filter = new GRCObjectFilter(new CurrentReportingPeriodImpl());
                 filter.setIncludeActiveWorkflows(true);
-
                 object = resourceService.getGRCObject(value, filter);
 
+                // Active workflows
                 List<Id> workflowProcess = object.getActiveWorkflowIds();
-
-                System.out.println("workflowProcess != null: " + (workflowProcess != null));
 
                 if (workflowProcess != null) {
                     for (Id processId : workflowProcess) {
+
                         List<IWFActivityInstance> instances = wf.getActivityInstances(processId);
                         IWFProcess wfProcess = wf.getProcess(processId);
                         List<IWFProcess> activeProcesses = wf.getStartedProcesses(value);
 
                         if (activeProcesses != null) {
                             for (IWFProcess iwfProcess : activeProcesses) {
-                                IWFProcess activeProcess = iwfProcess;
-                                System.out.println("Active Processes: " + activeProcess.toString());
-                                IWFActivityInstance currentActivityInstance = activeProcess.getCurrentActivityInstance();
-                                System.out.println("Current Activity Instance: " + currentActivityInstance);
+
+                                // Current activity instance
+                                IWFActivityInstance currentActivityInstance = iwfProcess.getCurrentActivityInstance();
 
                                 if (currentActivityInstance != null) {
 
-                                    IWFProcessDefinition proDef = wf.getProcessDefinitionByVersionId(activeProcess.getProcessVersionId());
-                                    IWFActivity activity = proDef.getActivity(currentActivityInstance.getActivityId());
+                                    // Passing processId from workFlowProcess List to be Set as a parameter to
+                                    // terminate the process
+                                    Set<Id> idSetWf = new HashSet<>();
+                                    idSetWf.add(processId);
 
-                                    System.out.println("***********************************************");
-                                    System.out.println("Stage Name: " + activity.getName());
+                                    // Terminating workflow process
+                                    wf.terminateProcesses(idSetWf);
 
                                 }
-
                             }
                         }
                     }
                 }
 
             }
-//
-//            IGRCObject grcObject = resourceService.getGRCObject(new Id("12501"));
-
-//
-//            System.out.println(grcObject.getName());
-
         } catch (Exception ex) {
             System.out.println("Exception occur: " + ex);
         }
 
+    }
+
+    public String queryBuilder() {
+
+        String query = "SELECT [KeyRiskIndicatorValue].[Resource ID] FROM [KeyRiskIndicatorValue] \" " + "+ \"WHERE [KeyRiskIndicatorValue].[IKI-KRIValues:FastMapUploaded] = 'Yes' \" " + "+ \"AND [KeyRiskIndicatorValue].[OPSS-KRI-Shared:Collection Status] = 'Collected'";
+
+        return query;
     }
 }
